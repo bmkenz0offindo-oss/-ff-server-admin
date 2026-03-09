0716419c55b52463b6473349cd6262ed2f020713
bmkenz0offindo-oss <bmkenz0offindo@gmail.com>
2026-03-09 14:05:30 +0700

    Create admin_duy.lua

---
+++ files/admin_duy.lua
@@ -0,0 +1,14 @@
+-- Source Mod OB52 by Admin Le Duy
+function Main()
+    -- Cấu hình nhẹ tâm (Headshot)
+    SetSensitivity(999)
+    SetAimFov(360)
+    SetHitboxScale("Head", 1.8) 
+    
+    -- Cấu hình Tay dài (Antenna)
+    SetAntennaType("Hand_Long")
+    SetAntennaColor("Yellow")
+    
+    -- Cấu hình súng
+    SetGunSkin("All", "Blue_Dragon_Effect")
+end
